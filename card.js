b5ef5f6f8093ccc1da771056e29a56f08da59417
Sasha <sasha.tsatryan.01@gmail.com>
2021-02-17 21:35:29 +0400

    some issues removed

--- card.js
+++
@@ -1,3 +0,0 @@
-let cards = [6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-let random = [7, 'Q', 10, 6, 8, 6, "J", 'J', 6, 'A', 'K'];
-console.log(random.sort((a, b) => cards.indexOf(a) - cards.indexOf(b)))
--- workWithJSON.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const myObj = JSON.parse(fs.readFileSync('1-json.json').toString());
-myObj.name = "Sasha";
-myObj.age = 19;
-fs.writeFileSync('1-json.json', JSON.stringify(myObj));
